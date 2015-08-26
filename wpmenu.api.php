452c74b5269681272f6f6202c528155c61b0070c
Matthew Dorman <matthewdorman@gmail.com>
2015-08-26 08:58:35 -0600

    Code Syntax cleanup

--- wpmenu.api.php
+++ wpmenu.api.php
@@ -44,108 +44,108 @@ use Drupal\Core\Url;
  * @ingroup wpmenu_tabs
  */
 function hook_wpmenu() {
-    $items = array();
+  $items = array();
 
-    // Add a search field to the wpmenu. The search field employs no wpmenu
-    // module theming functions.
-    $items['global_search'] = array(
-        '#type' => 'wpmenu_item',
-        'tab' => array(
-            '#type' => 'search',
-            '#attributes' => array(
-                'placeholder' => t('Search the site'),
-                'class' => array('search-global'),
-            ),
-        ),
-        '#weight' => 200,
-        // Custom CSS, JS or a library can be associated with the wpmenu item.
-        '#attached' => array(
-            'library' => array(
-                'search/global',
-            ),
-        ),
-    );
+  // Add a search field to the wpmenu. The search field employs no wpmenu
+  // module theming functions.
+  $items['global_search'] = array(
+    '#type' => 'wpmenu_item',
+    'tab' => array(
+      '#type' => 'search',
+      '#attributes' => array(
+        'placeholder' => t('Search the site'),
+        'class' => array('search-global'),
+      ),
+    ),
+    '#weight' => 200,
+    // Custom CSS, JS or a library can be associated with the wpmenu item.
+    '#attached' => array(
+      'library' => array(
+        'search/global',
+      ),
+    ),
+  );
 
-    // The 'Home' tab is a simple link, which is wrapped in markup associated
-    // with a visual tab styling.
-    $items['home'] = array(
-        '#type' => 'wpmenu_item',
-        'tab' => array(
-            '#type' => 'link',
-            '#title' => t('Home'),
-            '#url' => Url::fromRoute('<front>'),
-            '#options' => array(
-                'attributes' => array(
-                    'title' => t('Home page'),
-                    'class' => array('wpmenu-icon', 'wpmenu-icon-home'),
-                ),
-            ),
+  // The 'Home' tab is a simple link, which is wrapped in markup associated
+  // with a visual tab styling.
+  $items['home'] = array(
+    '#type' => 'wpmenu_item',
+    'tab' => array(
+      '#type' => 'link',
+      '#title' => t('Home'),
+      '#url' => Url::fromRoute('<front>'),
+      '#options' => array(
+        'attributes' => array(
+          'title' => t('Home page'),
+          'class' => array('wpmenu-icon', 'wpmenu-icon-home'),
         ),
-        '#weight' => -20,
-    );
+      ),
+    ),
+    '#weight' => -20,
+  );
 
-    // A tray may be associated with a tab.
-    //
-    // When the tab is activated, the tray will become visible, either in a
-    // horizontal or vertical orientation on the screen.
-    //
-    // The tray should contain a renderable array. An optional #heading property
-    // can be passed. This text is written to a heading tag in the tray as a
-    // landmark for accessibility.
-    $items['commerce'] = array(
-        '#type' => 'wpmenu_item',
-        'tab' => array(
-            '#type' => 'link',
-            '#title' => t('Shopping cart'),
-            '#url' => Url::fromRoute('cart'),
-            '#options' => array(
-                'attributes' => array(
-                    'title' => t('Shopping cart'),
-                ),
-            ),
+  // A tray may be associated with a tab.
+  //
+  // When the tab is activated, the tray will become visible, either in a
+  // horizontal or vertical orientation on the screen.
+  //
+  // The tray should contain a renderable array. An optional #heading property
+  // can be passed. This text is written to a heading tag in the tray as a
+  // landmark for accessibility.
+  $items['commerce'] = array(
+    '#type' => 'wpmenu_item',
+    'tab' => array(
+      '#type' => 'link',
+      '#title' => t('Shopping cart'),
+      '#url' => Url::fromRoute('cart'),
+      '#options' => array(
+        'attributes' => array(
+          'title' => t('Shopping cart'),
         ),
-        'tray' => array(
-            '#heading' => t('Shopping cart actions'),
-            'shopping_cart' => array(
-                '#theme' => 'item_list',
-                '#items' => array( /* An item list renderable array */ ),
-            ),
-        ),
-        '#weight' => 150,
-    );
+      ),
+    ),
+    'tray' => array(
+      '#heading' => t('Shopping cart actions'),
+      'shopping_cart' => array(
+        '#theme' => 'item_list',
+        '#items' => array( /* An item list renderable array */ ),
+      ),
+    ),
+    '#weight' => 150,
+  );
 
-    // The tray can be used to render arbitrary content.
-    //
-    // A renderable array passed to the 'tray' property will be rendered outside
-    // the administration bar but within the containing wpmenu element.
-    //
-    // If the default behavior and styling of a wpmenu tray is not desired, one
-    // can render content to the wpmenu element and apply custom theming and
-    // behaviors.
-    $items['user_messages'] = array(
-        // Include the wpmenu_tab_wrapper to style the link like a wpmenu tab.
-        // Exclude the theme wrapper if custom styling is desired.
-        '#type' => 'wpmenu_item',
-        'tab' => array(
-            '#type' => 'link',
-            '#theme' => 'user_message_wpmenu_tab',
-            '#theme_wrappers' => array(),
-            '#title' => t('Messages'),
-            '#url' => Url::fromRoute('user.message'),
-            '#options' => array(
-                'attributes' => array(
-                    'title' => t('Messages'),
-                ),
-            ),
+  // The tray can be used to render arbitrary content.
+  //
+  // A renderable array passed to the 'tray' property will be rendered outside
+  // the administration bar but within the containing wpmenu element.
+  //
+  // If the default behavior and styling of a wpmenu tray is not desired, one
+  // can render content to the wpmenu element and apply custom theming and
+  // behaviors.
+  $items['user_messages'] = array(
+    // Include the wpmenu_tab_wrapper to style the link like a wpmenu tab.
+    // Exclude the theme wrapper if custom styling is desired.
+    '#type' => 'wpmenu_item',
+    'tab' => array(
+      '#type' => 'link',
+      '#theme' => 'user_message_wpmenu_tab',
+      '#theme_wrappers' => array(),
+      '#title' => t('Messages'),
+      '#url' => Url::fromRoute('user.message'),
+      '#options' => array(
+        'attributes' => array(
+          'title' => t('Messages'),
         ),
-        'tray' => array(
-            '#heading' => t('User messages'),
-            'messages' => array(/* renderable content */),
-        ),
-        '#weight' => 125,
-    );
+      ),
+    ),
+    'tray' => array(
+      '#heading' => t('User messages'),
+      'messages' => array(/* renderable content */),
+    ),
+    '#weight' => 125,
+  );
 
-    return $items;
+  return $items;
 }
 
 /**
@@ -160,8 +160,8 @@ function hook_wpmenu() {
  *   Associative array of wpmenu menu definitions returned from hook_wpmenu().
  */
 function hook_wpmenu_alter(&$items) {
-    // Move the User tab to the right.
-    $items['commerce']['#weight'] = 5;
+  // Move the User tab to the right.
+  $items['commerce']['#weight'] = 5;
 }
 
 /**
